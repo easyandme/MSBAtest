5677ff719c22bb87eea4ebf238c7bb068715fee4
Maxim Xu <xujialv@gmail.com>
2016-03-28 16:00:37 +0800

    init

---
+++ js/script.js
@@ -0,0 +1,184 @@
+$(function() {
+
+  "use strict";
+
+$('body').bind('touchstart', function() {});
+  FastClick.attach(document.body);
+
+var q, k, m; //q for questions; k for question number in JSON; m for answer scores
+var l = 0; //l for progress
+var s = 0; //s for total scores
+var n = 1; //n for question number in test 
+var qar = []; //qar for questions array
+var playN = 0, rhythm = 0, sense = 0, memo = 0, deci = 0, know = 0;
+var r, w, x, y, z, o; // genre stars
+var isPlaying = false;
+
+/* append first song onto the board */
+function first() {
+    q = qar; 
+    k = 0;
+    $('.quiz_text>p').html(q[k].describe);
+}
+
+/* add overlay mask using pure js */
+function addOverlay() { 
+        var myOverlay = document.createElement('div');
+        myOverlay.id = 'overlay';
+        document.body.appendChild(myOverlay); 
+        myOverlay.style.position = 'absolute';
+        myOverlay.style.top = 0;  
+        myOverlay.style.opacity = 0.8;  
+        myOverlay.style.width = window.innerWidth + 'px';
+        myOverlay.style.height = window.innerHeight + 'px';
+        myOverlay.style.top = window.pageYOffset + 'px';
+        myOverlay.style.left = window.pageXOffset + 'px';
+        myOverlay.style.zIndex = 999;
+        myOverlay.style.backgroundColor = '#000'; 
+} 
+
+$('#start_btn').click(function() {
+      $('.prelude, .slogan').fadeOut(500);
+      $('#start').prepend(p);
+      $('#progressbar').fadeIn(1000);
+      setTimeout(function() {
+      $('.quiz').fadeIn(500);
+      }, 1000)
+      first();
+      $('#start_audio, .prelude').remove();
+})
+
+$('.choice').click(function() {
+      n += 1;
+      l += 100/13; 
+      $("#progressbar>div").css("width", l + "%");
+      var u = $(this).attr("data-choice");
+      o = q[k]; 
+      if (k < 13) {
+      m = o[u].bonus || 0;
+      s += m; 
+      r = o[u].r_score || 0;
+      rhythm += r;
+      w = o[u].sense_score || 0;
+      sense += w;
+      x = o[u].memo_score || 0;
+      memo += x;
+      y = o[u].decision_score || 0;
+      deci += y;
+      z = o[u].knowledge_score || 0;
+      know += z;  
+      $("#pg").text(k + 1 + "/13");
+      if ($('.quiz').hasClass('slideInRight animated')) {$('.quiz').removeClass('slideInRight animated')};
+      if ($('.play_btn>img').hasClass('spin begin')) {$('.play_btn>img').removeClass('spin begin')};
+      $('.quiz').addClass('slideOutLeft animatedFast'); 
+      setTimeout(function() {
+      k += 1;
+      $('.quiz_text>p').html(q[k].describe); 
+      isPlaying = true; 
+      $('.play_btn>img').addClass('spin begin'); 
+      if (q[k].hasOwnProperty('C')) {$('#choiceC').css('display', 'block')} else {$('#choiceC').css('display', 'none')};
+      if (q[k].hasOwnProperty('D')) {$('#choiceD').css('display', 'block');$('.choice').css('margin', '-6px auto')} else {$("#choiceD").css('display', 'none');$('.choice').css('margin', '2px auto')};
+      $("#choiceA").html(q[k].A.describe);
+      $("#choiceB").html(q[k].B.describe);
+      if (q[k].hasOwnProperty('C')) {$('#choiceC').html(q[k].C.describe)};
+      if (q[k].hasOwnProperty('D')) {$('#choiceD').html(q[k].D.describe)};
+      $(".quiz").removeClass('slideOutLeft animatedFast').addClass('slideInRight animated');
+      $('.intro').text(' ');
+      $('.play_btn>img').attr('src','./images/CD.png');
+      }, 200);
+      playN = 0;
+    }
+      if (n > 13) { 
+        $('.quiz, #progressbar').remove();
+        $('.result, .slogan').css('display', 'block').addClass('fadeInUp animatedSlow');
+      var pretext = ''; 
+      var perc, quotient, final_txt, middle;
+      //"if" statements look stupid? fastest though
+      if (s >= 90) {
+        s = Math.floor(Math.random()*9) + 90; 
+        perc = "申请BA牛校一申一个准"; 
+      } else if ( s > 70 && s < 90) { 
+        s = Math.floor(Math.random()*19) + 70; 
+        perc = "还差那么点儿意思";
+      } else if ( s > 50 && s <= 70) { 
+        s = Math.floor(Math.random()*19) + 50; 
+        perc = "哥们儿考虑转专业吧";
+      } else if (s > 10 && s <= 50) { 
+        s = Math.floor(Math.random()*39) + 10; 
+        perc = "哥们儿考虑转专业吧";
+      } else { 
+        perc = Math.floor(Math.random()*9) + 0; 
+        perc = "哥们儿咱能认真答吗";
+      }
+        quotient = s;
+        $('.percent').fadeIn(500);
+        $('#final_perc').text(perc);
+        $('.final').text(quotient).prop('counter', 0).animate({
+         counter: $('.final').text()
+      }, {
+        duration: 2000,
+        easing: "swing",
+        step: function(now) {
+            $(this).text(Math.ceil(now));
+          }
+      }); 
+      document.title = pretext + '我与BA的契合度' + middle + quotient + final_txt;
+      if (rhythm == 2) {
+        $('.a3').addClass('highlighted');
+      } else if (rhythm == 1) {
+        $('.a2').addClass('highlighted');  
+      } else {
+        $('.a1').addClass('highlighted');  
+      }
+      if (sense == 2) {
+        $('.b3').addClass('highlighted');
+      } else if (sense == 1) {
+        $('.b2').addClass('highlighted');  
+      } else {
+        $('.b1').addClass('highlighted');  
+      }
+      if (memo >= 3) {
+        $('.c3').addClass('highlighted');
+      } else if (memo > 1 && memo < 3) {
+        $('.c2').addClass('highlighted');  
+      } else {
+        $('.c1').addClass('highlighted');  
+      }
+      if (deci == 2) {
+        $('.d3').addClass('highlighted');
+      } else if (deci == 1) {
+        $('.d2').addClass('highlighted');  
+      } else {
+        $('.d1').addClass('highlighted');  
+      } 
+      if (know >= 3) {
+        $('.e3').addClass('highlighted');
+      } else if (know > 1) {
+        $('.e2').addClass('highlighted');  
+      } else {
+        $('.e1').addClass('highlighted');  
+      }
+    }
+});
+ 
+setInterval(function(){
+  $("#start_btn").toggleClass("shake animatedDelayed3");
+  $(".pointer").toggleClass("bounce animated"); 
+}, 2500);
+
+/*All the appending stuff*/
+var p = "<div hidden id='progressbar'><div><span id='pg'>1/13</span></div></div></div>";
+ 
+$.getJSON("data/data.json", function(e){
+        $.extend(qar, e);
+}); 
+
+$('.follow').click(function() { 
+        addOverlay(); 
+        $('img[alt="QR"], img[alt="des"]').show();
+        $('#overlay').click(function() {
+            $(this).remove(); 
+            $('img[alt="QR"], img[alt="des"], .share_txt').hide();
+        });
+});
+});
